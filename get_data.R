aa82aee37a1ccdb1e8799b474f6616a697da3e0b
Oliver Hofkens <oliver@novemberfive.co>
2017-07-14 22:16:03 +0200

    Basic project setup.

---
+++ get_data.R
@@ -0,0 +1,13 @@
+datadir <- 'data'
+datasource <- 'data/Coursera-SwiftKey.zip'
+dataset <- 'data/final'
+dir.create(datadir, showWarnings = FALSE)
+
+getData <- function(){
+    if(!dir.exists(dataset)){
+        if(!file.exists(datasource)){
+            download.file('https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip', datasource)
+        }
+        unzip(datasource, exdir = 'data')
+    }
+}
