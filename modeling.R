1ec8b599365cbf88a2b43c248f5e79e9f96d99ac
Oliver Hofkens <oliver@novemberfive.co>
2017-08-26 21:49:00 +0200

    Further refactoring of model.

--- modeling.R
+++ modeling.R
@@ -1,5 +1,7 @@
 source("modeling/loader.R")
 source("modeling/input.R")
+source("modeling/model.R")
+source("modeling/runner.R")
 library(tensorflow)
 
 modelConfig <- list(
@@ -17,240 +19,58 @@ modelConfig <- list(
     numSteps = 20L
 )
 
-languageModel <- function(){
-    epochSize <- 0L
-    cellLayers <- 0L
-    initialState <- 0L
-    finalState <- 0L
-    cost <- 0L
-    learningRate <- 0L
-    lrUpdate <- 0L
-    newLr <- 0L
-    lr <- 0L
-    num_steps <- 0L
-    trainOp <- 0L
-    
-    # Create an LSTM cell;
-    makeLstmCell <- function(){
-        tf$contrib$rnn$BasicLSTMCell(hidden_size, forget_bias=0.0)
-    }
-    
-    # Create an attention cell:
-    makeAttnCell <- function(isTraining){
-        cell <- makeLstmCell()
-        
-        if(isTraining && keep_prob < 1){
-            cell <- tf$contrib$rnn$DropoutWrapper(cell, output_keep_prob = keep_prob)
-        }
-        
-        cell
-    }
-    
-    # Layer multiple RNN cells together
-    makeLayeredCells <- function(isTraining, batchSize){
-        cellLayers <<- tf$contrib$rnn$MultiRNNCell(replicate(num_layers, makeAttnCell(isTraining)))
-        initialState <<- cellLayers$zero_state(batchSize, tf$float32)
-    }
-    
-    runModel <- function(isTraining, data, targets, batchSize = 20L, numSteps = 20L){
-        num_steps <<- numSteps
-        makeLayeredCells(isTraining, batchSize)
-        
-        # Create the embedding variable
-        embedding <- tf$get_variable("embedding", shape = shape(vocab_size, hidden_size), dtype = tf$float32)
-        # Lookup inputs:
-        inputs <- tf$nn$embedding_lookup(embedding, data)
-        
-        if (isTraining && keep_prob < 1){
-            inputs = tf$nn$dropout(inputs, keep_prob)
-        }
-           
-        outputs <- list()
-        state <- initialState
-        
-        with(tf$variable_scope("RNN"), {
-            for(time_step in seq.int(from = 0L, to = (numSteps - 1L))){
-                if(time_step > 0L){
-                    tf$get_variable_scope()$reuse_variables()
-                } 
-                results = cellLayers(inputs[, time_step, ], state)
-                outputs = c(outputs, results[1])
-                state = results[[2]]
-            }
-        })
-        
-        output = tf$reshape(tf$stack(axis=1L, values=outputs), list(-1L, hidden_size))
-        softmax_w = tf$get_variable("softmax_w", c(hidden_size, vocab_size), dtype=tf$float32)
-        softmax_b = tf$get_variable("softmax_b", c(vocab_size), dtype=tf$float32)
-        logits = tf$matmul(output, softmax_w) + softmax_b
-            
-        # Reshape logits to be 3-D tensor for sequence loss
-        logits = tf$reshape(logits, c(batchSize, numSteps, vocab_size))
-        
-        # use the contrib sequence loss and average over the batches
-        loss = tf$contrib$seq2seq$sequence_loss(
-            logits,
-            targets,
-            tf$ones(list(batchSize, numSteps), dtype=tf$float32),
-            average_across_timesteps=FALSE,
-            average_across_batch=TRUE
-        )
-        
-        # update the cost variables
-        cost <<- tf$reduce_sum(loss)
-        finalState <<- state
-        
-        if(!isTraining){
-            return(list(cost = cost))
-        }
-        
-        lr <<- tf$Variable(0.0, trainable=FALSE)
-        tvars = tf$trainable_variables()
-        grads = tf$clip_by_global_norm(tf$gradients(cost, tvars), max_grad_norm)
-        grads = grads[[1]]
-        optimizer = tf$train$GradientDescentOptimizer(lr)
-        zipped = mapply(c, grads, tvars, SIMPLIFY = FALSE)
-        trainOp <<- optimizer$apply_gradients(zipped, global_step=tf$contrib$framework$get_or_create_global_step())
-        
-        newLr <<- tf$placeholder(tf$float32, shape=c(), name="new_learning_rate")
-        lrUpdate <<- tf$assign(lr, newLr)
-        
-        list(cost = cost, lr = lr)
-    }
-    
-    assignLr <- function(session, value){
-        session$run(lrUpdate, feed_dict=dict(newLr = value))
-    }
-    
-    getLr <- function(){
-        lr
-    }
-    
-    getCost <- function(){
-        cost
-    }
-    
-    getInitialState <- function(){
-        initialState
-    }
-    
-    getFinalState <- function(){
-        finalState
-    }
-    
-    getNumSteps <- function(){
-        num_steps
-    }
-    
-    getTrainOp <- function(){
-        trainOp
-    }
-    
-    list(
-        runModel = runModel, 
-        assignLr = assignLr, 
-        getLr = getLr, 
-        getInitialState = getInitialState, 
-        getCost = getCost,
-        getFinalState = getFinalState,
-        getNumSteps = getNumSteps,
-        getTrainOp = getTrainOp
-        )
-}
-
-runEpoch <- function(session, model, epochSize, eval_op = NA, verbose = FALSE){
-    costs = 0.0
-    iters = 0L
-    state = session$run(model$getInitialState())
-    
-    fetches = dict(cost = model$getCost(), finalState = model$getFinalState())
-    if(!is.na(eval_op)){
-        fetches$evalOp <- eval_op
-    }
-    
-    for(i in seq.int(from = 0L, to = epochSize)){
-        feed_dict = dict()
-        
-        index = 1L
-        initialState <- model$getInitialState()
-        for(item in initialState){
-            c <- item$c
-            h <- item$h
-            feed_dict[[c]] = state[[index]]$c
-            feed_dict[[h]] = state[[index]]$h
-            index = index + 1
-        }
-        
-        vals = session$run(fetches, feed_dict)
-        cost = vals$cost
-        state = vals$finalState
-        
-        costs = costs + cost
-        iters = iters + model$getNumSteps()
-        
-        if(verbose && i %% (epochSize %/% 10) == 10){
-            cat(sprintf("%.3f perplexity: %.3f\n", i * 1.0 / epochSize, exp(costs / iters)))
-        }
-    }
-       
-    return(exp(costs/iters))
-}
+evalConfig <- modelConfig
+evalConfig$batchSize <- 1L
+evalConfig$numSteps <- 1L
 
 rawData <- loadModelInputs()
 detach('package:quanteda')
 
-trainInput <- LanguageModelInput(modelConfig, rawData$train, "TrainInput")
-
 with(tf$Graph()$as_default(), {
-    initializer = tf$random_uniform_initializer(-1 * init_scale, init_scale)
+    initializer = tf$random_uniform_initializer(-1 * modelConfig$initScale, modelConfig$initScale)
     
-    trainingInput <- batchData(train, 20L, 20L)
     with(tf$name_scope("Train"), {
+        trainInput <- LanguageModelInput(modelConfig, rawData$train, "TrainInput")
         with(tf$variable_scope("Model", reuse=FALSE, initializer=initializer),{
-            mTrain = languageModel()
-            results <- mTrain$runModel(isTraining = TRUE, data = trainingInput[[1]], targets = trainingInput[[2]])
-            tf$summary$scalar("Training Loss", results$cost)
-            tf$summary$scalar("Learning Rate", results$lr)
+            m <- LanguageModel(TRUE, modelConfig, trainInput)
+            tf$summary$scalar("Training Loss", m@cost)
+            tf$summary$scalar("Learning Rate", m@learningRate)
         })
     })
     
-    validationInput <- batchData(validation, 20L, 20L)
     with(tf$name_scope("Valid"), {
+        validInput <- LanguageModelInput(modelConfig, rawData$validation, "ValidationInput")
         with(tf$variable_scope("Model", reuse=TRUE, initializer=initializer),{
-            mValid = languageModel()
-            results <- mValid$runModel(isTraining = FALSE, data = validationInput[[1]], targets = validationInput[[2]])
-            tf$summary$scalar("Validation Loss", results$cost)
+            mValid <- LanguageModel(FALSE, modelConfig, validInput)
+            tf$summary$scalar("Validation Loss", mValid@cost)
         })
     })
     
-    testInput <- batchData(test, 1L, 1L)
     with(tf$name_scope("Test"), {
+        testInput <- LanguageModelInput(evalConfig, rawData$test, "TestInput")
         with(tf$variable_scope("Model", reuse=TRUE, initializer=initializer),{
-            mTest = languageModel()
-            results <- mTest$runModel(isTraining = FALSE, data = testInput[[1]], targets = testInput[[2]], 1L, 1L)
+            mTest <- LanguageModel(FALSE, evalConfig, testInput)
         })
     })
     
-    supervisor <- tf$train$Supervisor(logdir=getwd())
+    dir.create("log", showWarnings = FALSE)
+    supervisor <- tf$train$Supervisor(logdir=paste(getwd(), 'log', sep = "/"))
     
     with(supervisor$managed_session() %as% sess, {
-        for(i in seq.int(from = 0L, to = 12L)){
-            lr_decay = 0.5 ** max(i + 1 - 4, 0.0)
-            mTrain$assignLr(sess, 1 * lr_decay)
+        for(i in seq.int(from = 0L, to = (modelConfig$maxMaxEpoch - 1L))){
+            lrDecay = modelConfig$learningRateDecay ** max(i + 1 - modelConfig$maxEpoch, 0.0)
+            assignLearningRate(m, sess, modelConfig$learningRate * lrDecay)
             
-            #cat(sprintf("Epoch: %d Learning rate: %.3f\n", i + 1, sess$run(mTrain$getLr())))
-            #trainEpochSize <- ((length(train) %/% 20L) - 1) %/% 20L
-            #train_perplexity = runEpoch(sess, mTrain, epochSize = trainEpochSize, eval_op=mTrain$getTrainOp(), verbose=TRUE)
-            #cat(sprintf("Epoch: %d Train Perplexity: %.3f\n",  i + 1, train_perplexity))
+            cat(sprintf("Epoch: %d Learning rate: %.3f\n", i + 1, sess$run(m@learningRate))
+            trainPerplexity = runEpoch(sess, m, evalOp=m@trainOp, verbose=TRUE)
+            cat(sprintf("Epoch: %d Train Perplexity: %.3f\n",  i + 1, trainPerplexity))
             
-            validEpochSize <- ((length(validation) %/% 20L) - 1) %/% 20L
-            valid_perplexity = runEpoch(sess, mValid, validEpochSize)
-            cat(sprintf("Epoch: %d Valid Perplexity: %.3f\n", i + 1, valid_perplexity))
+            validerplexity = runEpoch(sess, mValid)
+            cat(sprintf("Epoch: %d Valid Perplexity: %.3f\n", i + 1, validPerplexity))
         }
         
-        testEpochSize <- ((length(test) %/% 1L) - 1) %/% 1L
-        test_perplexity = runEpoch(sess, mTest, testEpochSize)
-        cat(sprintf("Test Perplexity: %.3f\n", test_perplexity))
+        testPerplexity = runEpoch(sess, mTest)
+        cat(sprintf("Test Perplexity: %.3f\n", testPerplexity))
             
         print("Saving model\n")
         supervisor$saver$save(sess, paste(getwd(), 'model', sep = "/"), global_step=supervisor$global_step)
--- modeling/model.R
+++ modeling/model.R
@@ -1,7 +1,118 @@
+source("modeling/input.R")
+
 LanguageModel <- setClass(
     "LanguageModel",
     
     slots = c(
-        
+        input = "LanguageModelInput",
+        initialState = "ANY",
+        finalState = "ANY",
+        cost = "numeric",
+        learningRate = "numeric",
+        trainOp = "ANY",
+        newLearningRate = "ANY",
+        learningRateUpdate = "ANY"
     )
+)
+
+LanguageModel <- function(isTraining, config, input){
+    model <- new("LanguageModel", input = input)
+    
+    # Create an LSTM cell;
+    makeLstmCell <- function(){
+        tf$contrib$rnn$BasicLSTMCell(config$hiddenSize, forget_bias=0.0)
+    }
+    
+    # An attention cell is just a LSTM cell, unless we're in training and a keepProb below 100% was set:
+    makeAttnCell <- makeLstmCell
+    if(isTraining && config$keepProb < 1){
+        makeAttnCell <- function(){
+            tf$contrib$rnn$DropoutWrapper(cell, output_keep_prob = config$keepProb)
+        }
+    }
+    
+    # Create the multiple layered RNN cells
+    cell <- tf$contrib$rnn$MultiRNNCell(replicate(config$numLayers, makeAttnCell), state_is_tuple=TRUE)
+    model@initialState <- cell$zero_state(config$batchSize, tf$float32)
+    
+    # Create the embedding variable and lookup
+    embedding <- tf$get_variable("embedding", shape = shape(config$vocabSize, config$hiddenSize), dtype = tf$float32)
+    inputs <- tf$nn$embedding_lookup(embedding, input$inputData)
+    
+    # Add dropout if configured
+    if (isTraining && config$keepProb < 1){
+        inputs = tf$nn$dropout(inputs, config$keepProb)
+    }
+    
+    # Build the actual RNN layers
+    outputs <- list()
+    state <- model@initialState
+    
+    with(tf$variable_scope("RNN"), {
+        for(timeStep in seq.int(from = 0L, to = (config$numSteps - 1L))){
+            if(timeStep > 0L){
+                tf$get_variable_scope()$reuse_variables()
+            } 
+            results = cell(inputs[, timeStep, ], state)
+            outputs = c(outputs, results[1])
+            state = results[[2]]
+        }
+    })
+    
+    # Outputs
+    output = tf$reshape(tf$stack(axis=1L, values=outputs), list(-1L, config$hiddenSize))
+    softmaxW = tf$get_variable("softmax_w", c(config$hiddenSize, config$vocabSize), dtype=tf$float32)
+    softmaxB = tf$get_variable("softmax_b", c(config$vocabSize), dtype=tf$float32)
+    logits = tf$matmul(output, softmaxW) + softmaxB
+    
+    # Reshape logits to be 3-D tensor for sequence loss
+    logits = tf$reshape(logits, c(config$batchSize, config$numSteps, config$vocabSize))
+    
+    # use the contrib sequence loss and average over the batches
+    loss = tf$contrib$seq2seq$sequence_loss(
+        logits,
+        input$targetData,
+        tf$ones(list(config$batchSize, config$numSteps), dtype=tf$float32),
+        average_across_timesteps=FALSE,
+        average_across_batch=TRUE
+    )
+    
+    # Update the cost variables
+    model@cost <- tf$reduce_sum(loss)
+    model@finalState <- state
+    
+    if(!isTraining){
+        return(model)
+    }
+    
+    model@learningRate <- tf$Variable(0.0, trainable=FALSE)
+    tvars = tf$trainable_variables()
+    grads = tf$clip_by_global_norm(tf$gradients(model@cost, tvars), config$maxGradientNorm)
+    grads = grads[[1]]
+    optimizer = tf$train$GradientDescentOptimizer(model@learningRate)
+    zipped = mapply(c, grads, tvars, SIMPLIFY = FALSE)
+    model@trainOp <- optimizer$apply_gradients(zipped, global_step=tf$contrib$framework$get_or_create_global_step())
+    
+    model@newLearningRate <- tf$placeholder(tf$float32, shape=c(), name="new_learning_rate")
+    model@learningRateUpdate <- tf$assign(lr, newLr)
+    
+    model
+}
+
+# Define the generic method:
+setGeneric(name="assignLearningRate",
+          def=function(model, session, value)
+          {
+              standardGeneric("assignLearningRate")
+          }
+)
+
+# Implement it for our languageModel:
+setMethod(f="assignLearningRate",
+          signature="LanguageModel",
+          definition=function(model, session, value)
+          {
+              newLearningRate <- model@newLearningRate
+              session$run(model@learningRateUpdate, feed_dict=dict(newLearningRate = value))
+          }
 )
