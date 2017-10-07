ca6fbb9d9734bae48ab24ad1dc9cc1d7a9ec3ae5
Oliver Hofkens <oliver@novemberfive.co>
2017-10-07 16:13:34 +0200

    App; Translate words to IDs for model input.

--- app/server.R
+++ app/server.R
@@ -1,19 +1,35 @@
 library(shiny)
 library(tensorflow)
+library(data.table)
 
-vocab <- readRDS('vocab.rds')
 modelDir <- 'model/'
-checkpointName <- 'checkpoint'
-modelName <- 'model-157668.meta'
+vocab <- readRDS(paste('model','vocab.rds', sep = "/"))
+#checkpointName <- 'checkpoint'
+#modelName <- 'model-157668.meta'
 
-session <- tf$Session()
-saver <- tf$train$import_meta_graph(paste(modelDir, modelName, sep = ""))
-saver$restore(session, modelDir)
+#session <- tf$Session()
+#saver <- tf$train$import_meta_graph(paste(modelDir, modelName, sep = ""))
+#saver$restore(session, modelDir)
+
+getWordIds <- function(words){
+    ids <- vocab[.(words)]$id
+    
+    unk <- vocab[vocab$word == "<unk>"]$id
+    
+    # remove words that weren't found with <unk>
+    ids[is.na(ids)] <- unk
+    
+    ids
+}
 
 shinyServer(function(input, output) {
     
-    output$predictions <- renderText({
-        paste("<p>",getwd(),"</p>")
+    output$predictions <- renderPrint({
+        text <- tolower(input$text_input)
+        words <- unlist(strsplit(text, " ", fixed = TRUE))
+        ids <- getWordIds(words)
+        
+        print(ids)
     })
     
 })
