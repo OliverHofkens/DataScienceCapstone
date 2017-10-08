c987585d3adfe424c568db7a462743e875b0703c
Oliver Hofkens <oliver@novemberfive.co>
2017-10-08 18:48:04 +0200

    First steps for simpler model.

--- modeling.R
+++ modeling.R
@@ -1,96 +1,88 @@
 source("modeling/loader.R")
-source("modeling/input.R")
-source("modeling/model.R")
-source("modeling/runner.R")
 library(tensorflow)
 use_virtualenv("~/.virtualenvs/r-tensorflow/")
 
-# Testing config:
-modelConfig <- list(
-    initScale = 0.1,
-    learningRate = 1,
-    maxGradientNorm = 1L,
-    numLayers = 1L,
-    hiddenSize = 2L,
-    maxEpoch = 1L,
-    maxMaxEpoch = 1L,
-    keepProb = 1,
-    learningRateDecay = 0.5,
-    vocabSize = 10001L,
-    batchSize = 20L,
-    numSteps = 20L
-)
-
 # Actual config:
 modelConfig <- list(
-    initScale = 0.1,
-    learningRate = 1,
-    maxGradientNorm = 5L,
-    numLayers = 2L,
-    hiddenSize = 200L,
-    maxEpoch = 4L,
-    maxMaxEpoch = 13L,
-    keepProb = 1,
-    learningRateDecay = 0.5,
-    vocabSize = 10001L,
-    batchSize = 20L,
-    numSteps = 20L
+    numberOfWords = 5L, # Number of input words to base a prediction on.
+    learningRate = 0.001,
+    trainingIterations = 500L,
+    printInfoAfterXIterations = 100L,
+    nHiddenLayers = 200L,
+    vocabSize = 10001L
 )
-
-evalConfig <- modelConfig
-evalConfig$batchSize <- 1L
-evalConfig$numSteps <- 1L
 
 rawData <- loadModelInputs()
 detach('package:quanteda')
 
-with(tf$Graph()$as_default(), {
-    initializer = tf$random_uniform_initializer(-1 * modelConfig$initScale, modelConfig$initScale)
+tf$reset_default_graph()
+
+# Tensor that takes the input words
+inputTensor <- tf$placeholder(tf$float32, 
+                              shape(NULL, modelConfig$numberOfWords, 1L))
+# Tensor that holds the expected prediction
+rightPredictionTensor <- tf$placeholder(tf$float32, 
+                                        shape(NULL, modelConfig$vocabSize))
+
+# RNN output node weights and biases
+weights = dict(
+    'out' = tf$Variable(tf$random_normal(
+        shape(modelConfig$nHiddenLayers, modelConfig$vocabSize)
+        ))
+    )
+
+biases = dict(
+    'out' = tf$Variable(tf$random_normal(
+        shape(modelConfig$vocabSize)
+        ))
+)
+
+with(tf$variable_scope("RNN"), {
+    # Reshape the input to length-5 blocks
+    inputs <- tf$reshape(inputTensor, shape(-1L, modelConfig$numberOfWords))
     
-    with(tf$name_scope("Train"), {
-        trainInput <- LanguageModelInput(modelConfig, rawData$train, "TrainInput")
-        with(tf$variable_scope("Model", reuse=FALSE, initializer=initializer),{
-            m <- LanguageModel(TRUE, modelConfig, trainInput)
-            tf$summary$scalar("Training Loss", m@cost)
-            tf$summary$scalar("Learning Rate", m@learningRate)
-        })
-    })
+    # Split the inputs into subtensors of length 5
+    inputs <- tf$split(inputs, modelConfig$numberOfWords, 1L)
     
-    with(tf$name_scope("Valid"), {
-        validInput <- LanguageModelInput(modelConfig, rawData$validation, "ValidationInput")
-        with(tf$variable_scope("Model", reuse=TRUE, initializer=initializer),{
-            mValid <- LanguageModel(FALSE, modelConfig, validInput)
-            tf$summary$scalar("Validation Loss", mValid@cost)
-        })
-    })
+    # Stack 2 LSTM cells
+    rnnCell = tf$contrib$rnn$MultiRNNCell(list(
+        tf$contrib$rnn$BasicLSTMCell(modelConfig$nHiddenLayers),
+        tf$contrib$rnn$BasicLSTMCell(modelConfig$nHiddenLayers)
+    ))
     
-    with(tf$name_scope("Test"), {
-        testInput <- LanguageModelInput(evalConfig, rawData$test, "TestInput")
-        with(tf$variable_scope("Model", reuse=TRUE, initializer=initializer),{
-            mTest <- LanguageModel(FALSE, evalConfig, testInput)
-        })
-    })
+    # Generates predictions
+    results = tf$contrib$rnn$static_rnn(rnnCell, inputs, dtype=tf$float32)
+    outputs <- results[[1]]
+    state <- results[[2]]
     
-    dir.create("log", showWarnings = FALSE)
-    supervisor <- tf$train$Supervisor(logdir=paste(getwd(), 'log', sep = "/"))
-
-    with(supervisor$managed_session() %as% sess, {
-        for(i in seq.int(from = 0L, to = (modelConfig$maxMaxEpoch - 1L))){
-            lrDecay = modelConfig$learningRateDecay ** max(i + 1 - modelConfig$maxEpoch, 0.0)
-            assignLearningRate(m, sess, modelConfig$learningRate * lrDecay)
-
-            cat(sprintf("Epoch: %d Learning rate: %.3f\n", i + 1, sess$run(m@learningRate)))
-            trainPerplexity = runEpoch(sess, m, evalOp=m@trainOp, verbose=TRUE)
-            cat(sprintf("Epoch: %d Train Perplexity: %.3f\n",  i + 1, trainPerplexity))
-
-            validPerplexity = runEpoch(sess, mValid)
-            cat(sprintf("Epoch: %d Valid Perplexity: %.3f\n", i + 1, validPerplexity))
-        }
-
-        testPerplexity = runEpoch(sess, mTest)
-        cat(sprintf("Test Perplexity: %.3f\n", testPerplexity))
-
-        print("Saving model\n")
-        supervisor$saver$save(sess, paste(getwd(), 'model', sep = "/"), global_step=supervisor$global_step)
-    })
+    # there are numberOfWords outputs but we only want the last output
+    prediction <- tf$matmul(outputs[length(outputs)], weights[['out']]) + biases[['out']]
 })
+
+# Loss and optimizer
+cost <- tf$reduce_mean(tf$nn$softmax_cross_entropy_with_logits(
+    logits=prediction, labels=rightPredictionTensor)
+    )
+optimizer <- tf$train$RMSPropOptimizer(learning_rate=modelConfig$learningRate)$minimize(cost)
+
+# Compare the predictions with the correct values:
+correctPredictions <- tf$equal(tf$argmax(prediction, 1), tf$argmax(rightPredictionTensor,1))
+accuracy <- tf$reduce_mean(tf$cast(correctPredictions, tf$float32))
+
+# Initializer
+init <- tf$global_variables_initializer()
+
+# with(tf$Session %as% sess, {
+#     sess$run(init)
+#     
+#     step = 0L
+#     offset = random.randint(0,n_input+1)
+#     end_offset = n_input + 1
+#     totalAccuracy = 0
+#     totalLoss = 0
+#     
+#     while(step < modelConfig$trainingIterations){
+#         # Take numberOfWords and the correct next word:
+#         inputWords <- rawData
+#     }
+# })
