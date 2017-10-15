261b895b7019e0c6f5890dff861d40f184f6a97b
Oliver Hofkens <oliver@novemberfive.co>
2017-10-15 16:21:20 +0200

    Basic word prediction.

--- app/server.R
+++ app/server.R
@@ -1,21 +1,13 @@
 library(shiny)
 library(tensorflow)
+library(keras)
 library(data.table)
 
 # File names
-modelDir <- 'model'
-checkpointDir <- 'log'
-modelGraph <- 'model-2048540.meta'
-modelValues <- 'model-2048540'
+model <- load_model_hdf5('model/keras_model.h5')
 
 #Load Vocab
 vocab <- readRDS(paste('model','vocab.rds', sep = "/"))
-
-# Load RNN
-tf$reset_default_graph()
-sess <- tf$Session()
-saver <- tf$train$import_meta_graph(paste(modelDir, modelGraph, sep = "/"), clear_devices = TRUE)
-saver$restore(sess, paste(modelDir, modelValues, sep="/"))
 
 # Transforms a vector of words to a vector of the ids that represent those words.
 getWordIds <- function(words){
@@ -29,21 +21,36 @@ getWordIds <- function(words){
     ids
 }
 
-predict <- function(inputs){
-    graph <- tf$get_default_graph()
-    fetch = "Test/Model/Reshape_1"
-    outputs = sess$run(fetch, dict(inputs))
-    outputs
+getWordForId <- function(id){
+    select <- vocab$id == id
+    vocab[select]$word
+}
+
+predict_next_ids <- function(ids){
+    input <- sapply(vocab$id, function(x){
+        as.integer(x == ids)
+    })
+    dim(input) <- c(1, dim(input))
+    
+    preds <- predict(model, input)
+    
+    preds <- log(preds)
+    exp_preds <- exp(preds)
+    preds = exp_preds / sum(exp_preds)
+    
+    rmultinom(1, 1, preds) %>% 
+        as.integer() %>%
+        which.max()
 }
 
 shinyServer(function(input, output) {
     output$predictions <- renderPrint({
         text <- tolower(input$text_input)
         words <- unlist(strsplit(text, " ", fixed = TRUE))
+        words <- tail(words, n=5)
         ids <- getWordIds(words)
         
-        print(ids)
-        
-        results <- predict(ids)
+        prediction <- predict_next_ids(ids)
+        print(getWordForId(prediction))
     })
 })
