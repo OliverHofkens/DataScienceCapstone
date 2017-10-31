17442d6bfba4553c4f4f8641ccc29138875f5ae2
Oliver Hofkens <oliver@novemberfive.co>
2017-10-31 08:28:02 +0100

    Minor hyperparameter adjustments.

--- modeling.R
+++ modeling.R
@@ -7,21 +7,21 @@ config <- list(
     sequenceLengthWords = 10L,
     strideStep = 3L,
     embeddingSize = 200,
-    nHiddenLayers = 256,
-    learningRate = 0.005,
+    nHiddenLayers = 200,
+    learningRate = 0.001,
     batchSize = 100,
     nEpochs = 100,
     trainMaxQueueSize = 20,
-    lrDecay = 0.5,
+    lrDecay = 0.8,
     lrMin = 0.0001,
     decreaseLrPatience = 5,
     dropout = 0.2,
-    validationSplit = 0.2
+    validationSplit = 0.1
     )
 
 # Data Prep
 inputs <- loadModelInputs()
-train <- inputs$train[1:5000000]
+train <- inputs$train[1:10000000]
 #validation <- inputs$validation
 vocab <- inputs$vocabulary
 rm(inputs)
@@ -38,7 +38,6 @@ inputGenerator <- function(dataset, vocabulary, config, startAt=1) {
     index = startAt
     
     # Input = (batchSize x Sequence Length)
-    # Will be converted to Embedding by model.
     X <- array(0, dim = c(batchSize, seqLength))
     
     # 2-D output (batchSize x one-hot vocab)
@@ -47,7 +46,7 @@ inputGenerator <- function(dataset, vocabulary, config, startAt=1) {
     function() {
         nextIndex <-  index + (batchSize * stride) - 1
         
-        # If we reached the end (+ prediction), start over:
+        # If we reached the end (+ prediction), start over. Just in case.
         if(nextIndex + seqLength + 1 > length(dataset)){
             index <<- 1
             nextIndex <- index + (batchSize * stride) - 1
@@ -110,7 +109,7 @@ model %>%
                     mask_zero = TRUE, weights = list(embeddingMatrix)) %>%
     layer_lstm(config$nHiddenLayers, return_sequences = TRUE, 
                dropout = config$dropout) %>%
-    layer_lstm(config$nHiddenLayers) %>%
+    layer_lstm(config$nHiddenLayers, dropout = config$dropout) %>%
     layer_dense(length(vocab$id) + 1) %>%
     layer_activation("softmax")
 
@@ -120,7 +119,6 @@ model %>% compile(
     metrics = c('accuracy')
 )
 
-# Training and prediction
 history <- model %>%
     fit(
         x=inputDataset[[1]],
@@ -130,8 +128,8 @@ history <- model %>%
         validation_split = config$validationSplit,
         callbacks = list(
             callback_model_checkpoint("model.{epoch:02d}-{val_loss:.2f}.hdf5", save_best_only = TRUE),
-            callback_reduce_lr_on_plateau(monitor = "val_loss",factor = config$lrDecay, patience = config$decreaseLrPatience, min_lr = config$lrMin, cooldown = 2),
-            callback_tensorboard(log_dir = "log", embeddings_freq = 5, embeddings_metadata = 'vocab.tsv')
+            callback_reduce_lr_on_plateau(monitor = "val_loss",factor = config$lrDecay, patience = config$decreaseLrPatience, min_lr = config$lrMin),
+            callback_tensorboard(log_dir = "log", embeddings_freq = 10, embeddings_metadata = 'vocab.tsv')
             #callback_early_stopping(monitor = "val_loss", patience = 10)
         ))
 
