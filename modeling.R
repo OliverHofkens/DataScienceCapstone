1b91b3f5cf7f8d31229921507fcb034e40084274
Oliver Hofkens <oliver@novemberfive.co>
2017-11-11 14:23:22 +0100

    Minor adjustments.

--- modeling.R
+++ modeling.R
@@ -12,17 +12,18 @@ FLAGS <- flags(
     flag_numeric("nEpochs", 20),
     flag_numeric("lrDecay", 0.5),
     flag_numeric("lrMin", 0.001),
-    flag_numeric("decreaseLrPatience", 4),
+    flag_numeric("decreaseLrPatience", 3),
     flag_numeric("dropout1", 0.1),
     flag_numeric("dropout2", 0.1),
-    flag_numeric("sentencesPerBatch", 10000L),
+    flag_numeric("sentencesPerBatch", 20000L),
     flag_numeric("inputSentences", 100000L),
     flag_numeric("validationSentences", 1000L),
     flag_numeric("batchSize", 64),
     flag_string("continueFrom", FALSE),
     flag_boolean("trainEmbedding", FALSE),
     flag_string("weights", FALSE),
-    flag_numeric("startSuperEpoch", 1)
+    flag_numeric("startSuperEpoch", 1),
+    flag_numeric("topKMetric", 3)
 )
 
 # Data Prep
@@ -71,6 +72,10 @@ inputGenerator <- function(dataset, vocabulary, config) {
 
 embeddingMatrix <- readRDS('matrix.RDS')
 
+sparse_top_k_cat_acc <- function(y_pred, y_true){
+    metric_sparse_top_k_categorical_accuracy(y_pred, y_true, k = FLAGS$topKMetric)
+}
+
 # Model Definition
 if(FLAGS$continueFrom == "FALSE") {
     model <- keras_model_sequential()
@@ -81,12 +86,13 @@ if(FLAGS$continueFrom == "FALSE") {
                         mask_zero = TRUE, weights = list(embeddingMatrix),
                         trainable = FLAGS$trainEmbedding, 
                         name = 'embedding') %>%
-        layer_lstm(FLAGS$nHiddenLayers, return_sequences = TRUE, 
-                   dropout = FLAGS$dropout1, recurrent_dropout = FLAGS$dropout1,
-                   name = 'lstm-transfer-1') %>%
+#        layer_lstm(FLAGS$nHiddenLayers, return_sequences = TRUE, 
+#                   dropout = FLAGS$dropout1, recurrent_dropout = FLAGS$dropout1,
+#                   name = 'lstm-transfer-1') %>%
         layer_lstm(FLAGS$nHiddenLayers, 
                    dropout = FLAGS$dropout2, recurrent_dropout = FLAGS$dropout2,
                    name = 'lstm-last') %>%
+#        layer_batch_normalization() %>%
         layer_dense(length(vocab$id) + 1,
                     name = 'dense') %>%
         layer_activation("softmax",
@@ -94,14 +100,17 @@ if(FLAGS$continueFrom == "FALSE") {
     
     model %>% compile(
         loss = "sparse_categorical_crossentropy", 
-        optimizer = optimizer_nadam(lr = FLAGS$learningRate, clipnorm = 1),
-        metrics = c(top_k_acc = metric_sparse_top_k_categorical_accuracy)
+        optimizer = optimizer_nadam(lr = FLAGS$learningRate#, clipnorm = 1
+                                    ),
+        metrics = c(top_k_acc = sparse_top_k_cat_acc)
     )
 } else {
-    model <- load_model_hdf5(FLAGS$continueFrom)
+    model <- load_model_hdf5(FLAGS$continueFrom, custom_objects=c(top_k_acc=sparse_top_k_cat_acc))
 }
 
 if(FLAGS$weights != "FALSE"){
+    cat("Loading model weights:")
+    cat(FLAGS$weights)
     load_model_weights_hdf5(model, FLAGS$weights, by_name = TRUE)
 }
 
@@ -129,8 +138,8 @@ for(i in FLAGS$startSuperEpoch:superBatches){
             callbacks = list(
                 callback_model_checkpoint("model.{epoch:02d}-{val_top_k_acc:.2f}.hdf5", save_best_only = TRUE),
                 callback_reduce_lr_on_plateau(monitor = "top_k_acc",factor = FLAGS$lrDecay, patience = FLAGS$decreaseLrPatience, min_lr = FLAGS$lrMin, verbose=TRUE),
-                callback_tensorboard(log_dir = "log", embeddings_freq = 5, embeddings_metadata = 'vocab.tsv')
-                #callback_early_stopping(monitor = "val_loss", patience = 10)
+                callback_tensorboard(log_dir = "log", embeddings_freq = 10, embeddings_metadata = 'vocab.tsv'),
+                callback_early_stopping(monitor = "top_k_acc", patience = 10)
             ))
     
     save_model_weights_hdf5(model, paste('keras_weights', i, '.h5', sep = ""))
